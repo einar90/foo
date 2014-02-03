efd9fac9b7fff8fb6c9180539dba6800c8a162db
einar90 <einar.baumann@gmail.com>
2014-02-03 14:28:38 +0100

    Printing to console now works

--- Tables/python/LaTeXTablePrinter.py
+++ Tables/python/LaTeXTablePrinter.py
@@ -1,7 +1,31 @@
-""" This module generates LaTeX table code from input matrices. """
+"""
+This module contains functions that genrerate LaTeX table code
+from input matrices.
+"""
 
-class LaTeXTablePrinter:
-  array = []    # Array holding data to be printed
+from __future__ import print_function
+from numpy      import array as nparray
+from numpy      import shape
+from numpy      import append
 
-  def __init__(self, array):
+def print_latex_table(array):
+    """
+    Prints the LaTeX table code generated from the input array
+    to the console.
+    """
+    array = nparray(array)
+    dimen = array.shape
+
+    if len(dimen) != 2:
+        print('Can only generate tables for 2D arrays.')
+        return
+
+    for row in array:
+        row = append(row,'zo')
+        for element in row:
+            if element == 'zo':
+                printend = '\t\\\\\n'
+            else:
+                printend = '\t & '
+            print(element, end=printend)
     return
---
+++ Tables/python/LaTeXTablePrinterTest.py
@@ -0,0 +1,7 @@
+import LaTeXTablePrinter as ltp
+
+array = [ [1,2,3],
+          [4,5,6],
+          [7,8,9]]
+
+ltp.print_latex_table(array)
