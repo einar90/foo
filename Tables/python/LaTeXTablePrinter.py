3968f6b42ce5f9dd554e1ab55da900ab7946fff5
einar90 <einar.baumann@gmail.com>
2014-02-03 13:32:22 +0100

    Added python module to generate LaTeX tables

---
+++ Tables/python/LaTeXTablePrinter.py
@@ -0,0 +1,7 @@
+""" This module generates LaTeX table code from input matrices. """
+
+class LaTeXTablePrinter:
+  array = []    # Array holding data to be printed
+
+  def __init__(self, array):
+    return
